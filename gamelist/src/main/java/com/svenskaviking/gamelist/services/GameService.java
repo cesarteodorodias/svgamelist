4fa4f213ccb44373fb4c35d6bc8940896bf10a94
cesarteodorodias <cesaraugustoteodorodias@gmail.com>
2025-08-21 04:30:57 -0300

    GET by id

--- gamelist/src/main/java/com/svenskaviking/gamelist/services/GameService.java
+++ gamelist/src/main/java/com/svenskaviking/gamelist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.svenskaviking.gamelist.dto.GameDTO;
 import com.svenskaviking.gamelist.dto.GameMinDTO;
 import com.svenskaviking.gamelist.entities.Game;
 import com.svenskaviking.gamelist.repositories.GameRepository;
@@ -14,6 +16,13 @@ public class GameService {
 	
 	@Autowired
 	private GameRepository gameRepository;
+	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result =  gameRepository.findById(id).get();
+	    return new GameDTO(result);
+		
+	}
 	
 	public List<GameMinDTO> findAll(){		
 		List<Game> result = gameRepository.findAll();
